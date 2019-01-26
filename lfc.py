322ce95f9ea90d2a0aed1b67ced871effe6f507a
Ryan <ruli-92@protonmail.com>
2019-01-25 20:46:52 -0600

    added to code

--- lfc.py
+++ lfc.py
@@ -1,7 +1,17 @@
+# imports
 import json
+import random
 
+# vars
 file = 'eng_esp.json'
 
+# main
 words = json.loads(open(file).read())
+keys = list(words.keys())
+lang1 = keys[0]; del keys[0]
+lang2 = words[lang1]; del words[lang1]
+streak = 0
 
-print(words)
+print("Welcome to lang Flash Cards")
+print("Translate the words between " + lang1 + " and " + lang2)
+#while True:
