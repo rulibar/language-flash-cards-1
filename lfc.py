10a859ef99d932a575e57fac7f0ecf35577301f7
Ryan <ruli-92@protonmail.com>
2019-01-26 10:58:41 -0600

    completed base functionality on lfc.py. is now working.

--- lfc.py
+++ lfc.py
@@ -12,7 +12,17 @@ lang1 = keys[0]; del keys[0]
 lang2 = words[lang1]; del words[lang1]
 streak = 0
 
-print("Welcome to lang Flash Cards")
+print("Welcome to Language Flash Cards")
 print("Translate the words between " + lang1 + " and " + lang2)
 
-#while True:    
+while True:
+    key = random.choice(keys); word = words[key]
+    pair = [key, word]; random.shuffle(pair)
+
+    ans = input(pair[0] + " - ")
+    if ans == pair[1]:
+        streak += 1
+        print('Correct. Streak: {}. Next word:'.format(streak))
+    else:
+        streak = 0
+        print('Sorry, the correct answer is "{}".'.format(pair[1]))
