6ca7511a92e29f27f8855bb749d0741376f21e94
Ryan <ruli-92@protonmail.com>
2019-01-26 13:55:33 -0600

    exemplifies how to store multiple definitions for one word, or multiple words for one definition.

--- lfc.py
+++ lfc.py
@@ -5,24 +5,38 @@ import random
 # vars
 file = 'eng_esp.json'
 
-# main
-words = json.loads(open(file).read())
-keys = list(words.keys())
+# compile words list
+word_list = json.loads(open(file).read())
+keys = list(word_list.keys())
 lang1 = keys[0]; del keys[0]
-lang2 = words[lang1]; del words[lang1]
+lang2 = word_list[lang1]; del word_list[lang1]
 streak = 0
 
-print("Welcome to Language Flash Cards")
-print("Translate the words between " + lang1 + " and " + lang2)
+words = dict()
+new_keys = list(keys)
+for key in keys:
+    words[key] = set()
+    for item in word_list[key]:
+        words[key].add(item)
+        if item not in new_keys:
+            new_keys.append(item)
+            words[item] = {key}
+        else: words[item].add(key)
 
-while True:
-    key = random.choice(keys); word = words[key]
-    pair = [key, word]; random.shuffle(pair)
+print(words)
 
-    ans = input(pair[0] + " - ")
-    if ans == pair[1]:
-        streak += 1
-        print('Correct. Streak: {}. Next word:'.format(streak))
-    else:
-        streak = 0
-        print('Sorry, the correct answer is "{}".'.format(pair[1]))
+# main program
+#print("Welcome to Language Flash Cards")
+#print("Translate the words between " + lang2 + " and " + lang1)
+
+#while True:
+#    key = random.choice(keys); word = words[key]
+#    pair = [key, word]; random.shuffle(pair)
+#
+#    ans = input(pair[0] + " - ")
+#    if ans == pair[1]:
+#        streak += 1
+#        print('Correct. Streak: {}. Next word:'.format(streak))
+#    else:
+#        streak = 0
+#        print('Sorry, the correct answer is "{}".'.format(pair[1]))
