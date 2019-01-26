eaa15188fea754822a5da8da6e90fa8e47e550f4
Ryan <ruli-92@protonmail.com>
2019-01-26 14:31:23 -0600

    finish program such that each word can have multiple translation

--- lfc.py
+++ lfc.py
@@ -8,8 +8,9 @@ file = 'eng_esp.json'
 # compile words list
 word_list = json.loads(open(file).read())
 keys = list(word_list.keys())
-lang1 = keys[0]; del keys[0]
-lang2 = word_list[lang1]; del word_list[lang1]
+lang1 = keys[0].capitalize()
+lang2 = word_list[keys[0]].capitalize()
+del word_list[keys[0]]; del keys[0]
 streak = 0
 
 words = dict()
@@ -22,21 +23,21 @@ for key in keys:
             new_keys.append(item)
             words[item] = {key}
         else: words[item].add(key)
-
-print(words)
+keys = new_keys
 
 # main program
-#print("Welcome to Language Flash Cards")
-#print("Translate the words between " + lang2 + " and " + lang1)
+print("Welcome to Language Flash Cards.")
+print("Translate the words between {} and {}.".format(lang2, lang1))
 
-#while True:
-#    key = random.choice(keys); word = words[key]
-#    pair = [key, word]; random.shuffle(pair)
-#
-#    ans = input(pair[0] + " - ")
-#    if ans == pair[1]:
-#        streak += 1
-#        print('Correct. Streak: {}. Next word:'.format(streak))
-#    else:
-#        streak = 0
-#        print('Sorry, the correct answer is "{}".'.format(pair[1]))
+while True:
+    key = random.choice(keys)
+    attempt = input(key + " - ")
+    answer = key.capitalize() + ": "
+    for item in words[key]: answer += item + ", "
+    answer = answer[:-2]
+    if attempt in words[key]:
+        streak += 1
+        print('Correct. {}. Streak: {}'.format(answer, streak))
+    else:
+        streak = 0
+        print('Incorrect. {}.'.format(answer))
